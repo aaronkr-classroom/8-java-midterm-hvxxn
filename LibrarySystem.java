d243278a1e50c2fb9345109affcb59aee0c1a96d
전태현 <hyeon041004@gmail.com>
2024-04-23 15:37:25 +0900

    1

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,7 +1,7 @@
 public class LibrarySystem {
 	// 변수 정의
 	
-	// 생성자 정
+	// 생성자 정의
 
 	public void addBook(Book book) {
 		// 책 추가
@@ -24,54 +24,52 @@ public class LibrarySystem {
 				booklist[--bookCount] = null;
 				System.out.println("라이브러리에서 '" + title + "' 삭제했습니다.");
         
-				return; // 책 제거 후 종료 방법
+				return;
 			}
 		}
-		// 찾을 수 없으면 메시지를 인쇄합니다.
+
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
 	}
 	
 	public void borrowBook(String title) {
-        // `for` LibrarySystem의 모든 Book {
+		for ( int i = 0; i < bookCount; i++ ){
+
             // `if` 책 제목이 발견되면 {
+			if ( booklist[i].getTitle().equals(title)) {	
                 // `if` 책이 있다면 {
-                    // setAvailable을 false로 설정하고 메시지를 출력합니다.
-				// } `else` 책이 없다면 {
-					// 메시지를 인쇄하다
-				// }
-				return; // 책 제거 후 종료 방법
-			// }
-		// }
-		// 찾을 수 없으면 메시지를 인쇄합니다.
+				if ( !booklist[i].isAvailable()) {
+					return false;
+				} else { System.out.println("책을 찾았습니다");
+				
+					
+				}
+			}
+			return;
+		}
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
 	}
+
 	
 	public void returnBook(String title) {
-	    // `for` LibrarySystem의 모든 Book {
-            // `if` 책 제목이 발견되면 {
-                // `if` 책이 없다면 {
-                    // setAvailable을 true로 설정하고 메시지를 출력합니다.
-				// } `else` 책이 있다면 {
-					// 메시지를 인쇄하다
-				// }
-				return; // 책 제거 후 종료 방법
-			// }
-		// }
-		// 찾을 수 없으면 메시지를 인쇄합니다.
+		for ( int i = 0; i < bookCount; i++ ){
+			if ( booklist[i].getTitle().equals(title)) {	
+				if ( !booklist[i].isAvailable()) {
+					return true;
+				} else { System.out.println("책을 찾았습니다");
+				
+					
+				}
+			}
+			return;
+		}
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
 	}
 	
 	public void displayAllBooks() {
-		// 책 목록 출력 (책이름, 저자, 출판년도, 대출가능여부 출력)
-
-        /* 
-        Expected output:
-        "ISBN1234 | JSP 웹 프로그래밍 | 송미영 | 2018 | Available"
-        "ISBN1235 | 안드로이드 프로그래밍 | 우재남 | 2022 | Out"
-        ...
-        */
-    
-        // 도움을 위해 Cart.java의 printBookList()를 참조하세요
+		Systen.out.println("ISBN1234 | JSP 웹 프로그래밍 | 송미영", 2018);
+		Systen.out.println("ISBN1235 | 안드로이드 프로그래밍 | 우재남", 2022);
+		Systen.out.println("ISBN1236 | 스크래치 프로그래밍 | 고광일", 2019);
+		Systen.out.println("ISBN1237 | JAVA 마스터 | 송미영", 2023);
     }
 
 }
